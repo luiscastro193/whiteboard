836bcdf86ee8659bba3fc8658e4eeeaf3ff9c7c6
Luis Castro Martín <luiscastro193@gmail.com>
2026-04-18 16:06:48 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+"use strict";
+const main = document.querySelector('main');
+const root = document.querySelector(':root');
+const text = document.createRange();
+
+function fits(mainRect) {
+	const textRect = text.getBoundingClientRect();
+	return textRect.width <= mainRect.width && textRect.height <= mainRect.height;
+}
+
+function setFactor() {
+	localStorage.setItem("whiteboard", main.textContent);
+	const mainRect = main.getBoundingClientRect();
+	text.selectNodeContents(main);
+	let factor = 1;
+	root.style.setProperty('--factor', factor);
+	if (!main.textContent || fits(mainRect)) return;
+	let low = 0, high = 1;
+	
+	while (high - low > 1e-4) {
+		factor = (low + high) / 2;
+		root.style.setProperty('--factor', factor);
+		if (fits(mainRect)) low = factor;
+		else high = factor;
+	}
+	
+	root.style.setProperty('--factor', low);
+}
+
+if (!main.textContent) main.textContent = localStorage.getItem("whiteboard");
+await document.fonts.ready;
+main.oninput = setFactor;
+new ResizeObserver(setFactor).observe(main);
