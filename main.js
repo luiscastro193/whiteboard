950c79f634abe2ba7239444037ef384a3585b9ea
luiscastro193 <luiscastro193@gmail.com>
2026-04-19 13:01:31 +0200

    Bug fixes

--- main.js
+++ main.js
@@ -1,32 +1,22 @@
 "use strict";
 const main = document.querySelector('main');
-const text = document.createRange();
 let startY;
 
-function fits(mainRect) {
-	const textRect = text.getBoundingClientRect();
-	return textRect.width <= mainRect.width && textRect.height <= mainRect.height;
+function fits() {
+	return main.scrollWidth <= main.clientWidth && main.scrollHeight <= main.clientHeight;
 }
 
 function setFactor() {
-	localStorage.setItem("whiteboard", main.textContent);
+	localStorage.setItem("whiteboard", main.innerHTML);
 	let factor = 1;
 	main.style.setProperty('--factor', factor);
-	
-	if (!main.textContent) {
-		main.innerHTML = '';
-		return;
-	}
-	
-	const mainRect = main.getBoundingClientRect();
-	text.selectNodeContents(main);
-	if (fits(mainRect)) return;
+	if (fits()) return;
 	let low = 0, high = 1;
 	
 	while (high - low > 1e-4) {
 		factor = (low + high) / 2;
 		main.style.setProperty('--factor', factor);
-		if (fits(mainRect)) low = factor;
+		if (fits()) low = factor;
 		else high = factor;
 	}
 	
@@ -45,7 +35,7 @@ document.addEventListener('keydown', event => {
 	if (event.key == 'Escape') main.blur();
 });
 
-if (!main.textContent) main.textContent = localStorage.getItem("whiteboard");
+if (!main.innerHTML) main.innerHTML = localStorage.getItem("whiteboard");
 
 await document.fonts.ready;
 main.oninput = setFactor;
